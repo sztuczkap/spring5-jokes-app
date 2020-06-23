310a2365d8bfcc2f7c813923aa4167dd21183775
sztuczkap <p.sztuczka@wp.pl>
2020-06-23 18:19:53 +0200

    first commit

---
+++ src/test/java/sztuczkap/springframework/joke/joke/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package sztuczkap.springframework.joke.joke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
